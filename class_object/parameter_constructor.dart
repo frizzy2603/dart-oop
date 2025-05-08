ecc76be43b5441643fb1a297732f6b8a71006326
frizzy2603 <Tololboy05@'gmail.com>
2025-05-08 13:24:18 +0700

    update dart oop

--- class_object/parameter_constructor.dart
+++ class_object/parameter_constructor.dart
@@ -1,16 +1,16 @@
 class Student {
-    String? name;
-    int? age;
-    int? rollNumber;
+  String? name;
+  int? age;
+  int? rollNumber;
 
-    //konstruktor
-    Student(this.name, this.age, this.rollNumber);
+  //konstruktor
+  Student(this.name, this.age, this.rollNumber);
 }
 
-void main(){
-    //disini student adalah object dari class Student
-    Student student = Student("Angga", 20, 1);
-    print("Name: ${student.name}");
-    print("Age: ${student.age}");
-    print("Roll Number: ${student.rollNumber}");
+void main() {
+  //disini student adalah object dari class Student
+  Student student = Student("frizzy", 20, 1);
+  print("Name: ${student.name}");
+  print("Age: ${student.age}");
+  print("Roll Number: ${student.rollNumber}");
 }
